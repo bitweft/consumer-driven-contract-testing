d89523fa67a5dfbe6013419ee2309d94ad99d4a5
bitweft <bitweft@gmail.com>
2019-06-06 08:33:46 +0800

    Add JenkinsFile for consumer

---
+++ user-privilege-service/src/test/java/user_privilege_engine/unit/DummyTest.java
@@ -0,0 +1,12 @@
+package user_privilege_engine.unit;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class DummyTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1);
+    }
+}
